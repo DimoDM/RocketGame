ffcf8cdef4e5a6976cf112fe1a58a0fb6c8172cd
DimoDM <dimo_marino@abv.bg>
2020-11-19 15:02:23 +0200

    set up application component

---
+++ app/src/main/java/com/example/rocketgame/RocketGameApplication.java
@@ -0,0 +1,31 @@
+package com.example.rocketgame;
+
+import android.app.Application;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasAndroidInjector;
+
+public class RocketGameApplication  extends Application implements
+        HasAndroidInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Object> activityDispatchingAndroidInjector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DaggerApplicationComponent.builder()
+                .applicationBind(this)
+                .build()
+                .inject(this);
+    }
+
+    @Override
+    public AndroidInjector<Object> androidInjector() {
+        return activityDispatchingAndroidInjector;
+    }
+}
---
+++ app/src/main/java/com/example/rocketgame/di/modules/ContractsModule.java
@@ -0,0 +1,4 @@
+package com.example.rocketgame.di.modules;
+
+public class ContractsModule {
+}
