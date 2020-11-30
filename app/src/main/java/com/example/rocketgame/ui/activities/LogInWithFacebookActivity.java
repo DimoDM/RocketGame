d661769cb0e2f375038c25031766c5d25748dce8
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2020-11-30 18:46:24 +0200

    Added app secret and app Id and made changes in manisfest file

---
+++ app/src/main/java/com/example/rocketgame/ui/activities/LogInWithFacebookActivity.java
@@ -0,0 +1,16 @@
+package com.example.rocketgame.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rocketgame.R;
+
+public class LogInWithFacebookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_with_facebook);
+    }
+}
