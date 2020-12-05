bbd4af3ddce74321e97df41aeb074fb0128bcef6
DimoDM <dimo_marino@abv.bg>
2020-12-05 09:36:59 +0200

    addIcon

--- app/src/main/java/com/example/rocketgame/ui/fragments/ClassationFragment.java
+++ app/src/main/java/com/example/rocketgame/ui/fragments/ClassationFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.rocketgame.R;
+import com.example.rocketgame.RocketGameApplication;
 import com.example.rocketgame.core.contract.ClassationFragmentContract;
 import com.example.rocketgame.core.contract.GameFragmentContract;
 import com.example.rocketgame.databinding.FrgmentClassationBinding;
@@ -13,7 +14,7 @@ import com.example.rocketgame.ui.adapter.ClassationAdapter;
 
 import javax.inject.Inject;
 
-import static com.facebook.FacebookSdk.getApplicationContext;
+
 
 public class ClassationFragment extends BaseFragment<FrgmentClassationBinding, ClassationFragment.OnClassationFragmentInteractionListener>
 implements ClassationFragmentContract.ViewListener{
@@ -29,7 +30,7 @@ implements ClassationFragmentContract.ViewListener{
     @Override
     protected void onFragmentCreated(View view, Bundle savedInstanceState) {
         presentListener.setViewListener(this);
-        binding.resView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+        binding.resView.setLayoutManager(new LinearLayoutManager(RocketGameApplication.APP.getApplicationContext(), LinearLayoutManager.VERTICAL, false));
         ClassationAdapter adapter = new ClassationAdapter();
         binding.resView.setAdapter(adapter);
         binding.backToMenu.setOnClickListener(v -> presentListener.onHomeBtnClicked());
